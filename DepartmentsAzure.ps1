98d5b15040699760195521274f585652b420f46a
Anton Petrianik <58851987+tato1027@users.noreply.github.com>
2021-11-16 16:17:51 +0300

    Create DepartmentsAzure.ps1

---
+++ DepartmentsAzure.ps1
@@ -0,0 +1,20 @@
+$ou = "" #Distinguished name of CN with users
+$users = Get-ADUser -Filter * -SearchBase $ou -Properties Department, extensionAttribute11 | Select-Object SamAccountName, Department, extensionAttribute11
+
+function Copy-DepartmentAttribute {
+    param($user)
+        
+    if ($user.Department -ne $null -and $user.Department.Length -gt 64) {
+        $department = $user.Department.Substring(0,64)
+    }
+    elseif($user.Department -ne $null -and $user.Department.Length -le 64) {
+        $department = $user.Department    
+    }  
+       
+    $thisUser = Get-ADUser -Identity $user.SamAccountName -Properties extensionAttribute11
+    Set-ADUser –Identity $thisUser -add @{"extensionattribute11"=$department.ToString()} -Verbose
+}
+
+foreach ($user in $users) {
+    Copy-DepartmentAttribute -user $user
+}
